21344210793b13a833586a227f1a303d181d2949
Sunil Kumar <sunil.kumar2@nike.com>
2021-05-07 10:47:23 -0700

    bump

---
+++ tests/conftest.py
@@ -0,0 +1,15 @@
+import pytest
+
+class Movie:
+
+    def __init__(self, title, year, plot, rating):
+        self.title = title
+        self.year = year
+        self.plot = plot
+        self.rating = rating
+
+
+@pytest.fixture(scope='session', autouse=True)
+def put_Movies_str():
+    return Movie("The Big New Movie",2015,
+                           "Nothing happens at all.", 0)
--- tests/test_movies.py
+++ tests/test_movies.py
@@ -1,19 +1,36 @@
-import simplejson as json
-from pprint import pprint
 import unittest
+from pprint import pprint
 from unittest import mock
+
 import boto3
+import pytest
+import simplejson as json
 from botocore.exceptions import ClientError
 from moto import mock_dynamodb2
 
+
 @mock_dynamodb2
 class TestDatabaseFunctions(unittest.TestCase):
+    
     def setUp(self):
         """Create the mock database and table"""
         self.dynamodb = boto3.resource('dynamodb', region_name='us-east-1')
         
         from myapp.api.repository.MoviesCreateTable import create_movie_table
+
         self.table = create_movie_table(self.dynamodb) 
+
+        self.expeted_response={
+            'Items':[
+                {
+                "year": 2015,
+                "info": {
+                    "rating": 0,
+                    "plot": "Nothing happens at all."
+                },
+                "title": "The Big New Movie"}
+            ]
+        }
     
     def tearDown(self):
         """Delete mock database and table after test is run"""
@@ -37,8 +54,8 @@ class TestDatabaseFunctions(unittest.TestCase):
 
     def test_3_get_movie(self):
        """Test the get function"""
-       from myapp.api.repository.movies import put_movie ,get_movie
-       result1 = put_movie("The Big New Movie",2015,
+       from myapp.api.repository.movies import get_movie, put_movie
+       put_movie("The Big New Movie",2015,
                            "Nothing happens at all.", 0)
        result = get_movie(2015,"The Big New Movie")
 
@@ -48,27 +65,18 @@ class TestDatabaseFunctions(unittest.TestCase):
        self.assertEqual(0, result['info']['rating'])
 
     def test_4_get_movie_api(self):
-        from myapp.api.repository.movies import put_movie ,get_movie
-        result1 = put_movie("The Big New Movie",2015,
+        """Test the get api with path parameter function"""
+        from myapp.api.repository.movies import get_movie, put_movie
+        put_movie("The Big New Movie",2015,
                            "Nothing happens at all.", 0)
-        expeted_response={
-            'Items':[
-                {
-                "year": 2015,
-                "info": {
-                    "rating": 0,
-                    "plot": "Nothing happens at all."
-                },
-                "title": "The Big New Movie"}
-            ]
-        }
+        
      
         from fastapi.testclient import TestClient
         from myapp.main import app
         client = TestClient(app)   
         response = client.get("/v1/movies/2015")
         assert response.status_code == 200
-        assert response.json() == expeted_response['Items']
+        assert response.json() == self.expeted_response['Items']
 
 
     # @mock.patch("myapp.api.repository.movies.table.query") 
